5b7357f8181047fa5a16c16b07571a0942631613
GabrielBdn <gabriel.beduneau1@gmail.com>
2025-04-30 15:50:26 +0200

    V1

--- storebrain-frontend/app/[lang]/(dashboard)/layout.tsx
+++ storebrain-frontend/app/[lang]/(dashboard)/layout.tsx
@@ -7,11 +7,18 @@ import { auth } from "@/lib/auth";
 const layout = async ({ children, params: { lang } }: { children: React.ReactNode; params: { lang: any } }) => {
   const session = await auth();
 
+  console.log(session);
+
+
   if (!session?.user) {
+    console.log(session?.user);
     redirect("/auth/login");
+
+    
   }
 
   const trans = await getDictionary(lang);
+  console.log(trans);
 
   return (
     <DashBoardLayoutProvider trans={trans}>{children}</DashBoardLayoutProvider>
